abc9f7f6a2e219564dd7d36631e78d1229189051
jose manuel villar alarcón <jmvalarcon@gmail.com>
2022-05-14 08:25:50 -0300

    response

--- src/operators.rxjs.ts
+++ src/operators.rxjs.ts
@@ -1,16 +1,17 @@
 import { AxiosResponse } from "axios";
 import { Observable, of } from "rxjs";
-import { map } from "rxjs/operators";
-import { catchError, tap } from "rxjs/operators";
+import { map, retry } from "rxjs/operators";
+import { catchError } from "rxjs/operators";
 import { IHttpResponse } from "./interface";
 
 
 export const ResponsePipago = (source$: Observable<any>): Observable<any> =>
     source$
         .pipe(
+            retry(2),
             catchError((err) => {
-                console.log(err)
-                return of(err);
+                console.log('', err.code)
+                return of({ error_desc: err.code, error: true });
             }),
             map((response: AxiosResponse) => response?.data || response),
             map((response: IHttpResponse) => response.error ? response : response.data),
